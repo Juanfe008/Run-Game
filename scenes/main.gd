5e26de0b76b0e5eb76080ccfc2b024ab26f3b6a4
Juanfe008 <54733201+Juanfe008@users.noreply.github.com>
2025-01-15 18:49:12 -0500

    mainScript: Obstacle spawner

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,14 @@
 extends Node
 
+# Preload obstacles
+var stump_scene = preload("res://scenes/stump.tscn")
+var rock_scene = preload("res://scenes/rock.tscn")
+var barrel_scene = preload("res://scenes/barrel.tscn")
+var bird_scene = preload("res://scenes/bird.tscn")
+var obstacle_types := [stump_scene, rock_scene, barrel_scene]
+var obstacles : Array
+var bird_heights := [200, 390]
+
 # Game variables
 const PLAYER_START_POS := Vector2(150, 485)
 const CAM_START_POS := Vector2(576, 324)
@@ -10,12 +19,15 @@ const START_SPEED : float = 10.0
 const MAX_SPEED : int = 25
 const SPEED_MODIFIER: int = 5000
 var screen_size : Vector2i
+var ground_height : int
 var game_running : bool
+var last_obs
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_window().size
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
 
 func new_game() -> void:
@@ -40,6 +52,9 @@ func _process(delta: float) -> void:
 		if speed > MAX_SPEED:
 			speed = MAX_SPEED
 		
+		# Generate obstacles
+		generate_obs()
+		
 		# Move player and camera
 		$Player.position.x += speed
 		$Camera2D.position.x += speed
@@ -56,5 +71,25 @@ func _process(delta: float) -> void:
 			game_running = true
 			$HUD.get_node("StartLabel").hide()
 
+func generate_obs():
+	#generate ground obstacles
+	if obstacles.is_empty() or last_obs.position.x < score + randi_range(300, 500):
+		var obs_type = obstacle_types[randi() % obstacle_types.size()]
+		var obs
+		var max_obs = 3
+		for i in range(randi() % max_obs + 1):
+			obs = obs_type.instantiate()
+			var obs_height = obs.get_node("Sprite2D").texture.get_height()
+			var obs_scale = obs.get_node("Sprite2D").get_scale()
+			var obs_x : int = screen_size.x + score + 100 + (i * 100)
+			var obs_y : int = screen_size.y - ground_height - (obs_height * obs_scale.y /2) + 5
+			last_obs = obs
+			add_obs(obs, obs_x, obs_y)
+
+func add_obs(obs, x, y):
+		obs.position = Vector2i(x, y)
+		add_child(obs)
+		obstacles.append(obs)
+	
 func show_score():
 	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER) 
