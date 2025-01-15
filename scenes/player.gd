22a6e9180477f403caa4f4419bede19d984433c1
Juanfe008 <54733201+Juanfe008@users.noreply.github.com>
2025-01-14 19:57:45 -0500

    playerScript: Basic movement

---
+++ scenes/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const GRAVITY = 4200
+const JUMP_VELOCITY = -1800
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	velocity.y += GRAVITY * delta
+
+	# Handle movement.
+	if is_on_floor():
+		$RunningCol.disabled = false
+		if Input.is_action_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+			$JumpSound.play()
+		elif Input.is_action_pressed("ui_down"):
+			$AnimatedSprite2D.play("duck")
+			$RunningCol.disabled = true
+		else:
+			$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("jump")
+	move_and_slide()
